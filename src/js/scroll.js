3ba77df7afec1134ae5584fdbba815a2ab68e11d
Bill Wilson Yede Nka <bill9414@hotmail.com>
2019-03-11 23:44:43 +0100

    hi

---
+++ src/js/scroll.js
@@ -0,0 +1,60 @@
+const Container = document.querySelector(".bill")
+const pics = document.querySelector(".picture")
+let currentPixel = window.pageYOffset
+
+const loop = () => {
+    const newPixel = window.pageYOffset
+    const diff = newPixel - currentPixel
+    const speed = diff * 0.25
+
+    pics.style.transform = "skewY(" + speed + "deg)"
+
+
+    currentPixel = newPixel
+
+    requestAnimationFrame(loop)
+}
+
+loop();
+
+var rellax = new Rellax('.header')
+var lerellax = new Rellax('.Form')
+
+
+const animate = document.querySelectorAll(".project2, .bill, .unble");
+
+animate.forEach(element => {
+    element.style.opacity = 0
+});
+
+const fadeIn = function () {
+
+
+    let delay = 0.25;
+
+    animate.forEach(element => {
+        const elementTop = element.getBoundingClientRect().top
+        const elementBottom = element.getBoundingClientRect().bottom
+
+        if(elementTop < window.innerHeight) {
+            element.style.animation = `fadein 1s ${delay}s both`
+            delay = delay + 0.25;
+        }
+    })
+}
+fadeIn();
+
+document.addEventListener("scroll", function (){
+    fadeIn()
+})
+
+window.addEventListener("resize", function(){
+    fadeIn()
+})
+
+
+
+
+
+
+
