0bb925caa29871932f2647d0ba93f0dcf01791a5
Bill Wilson Yede Nka <bill9414@hotmail.com>
2019-03-13 10:46:39 +0100

    change

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,6 +1,27 @@
 const Container = document.querySelector(".bill")
 const pics = document.querySelector(".picture")
 let currentPixel = window.pageYOffset
+$('document').ready(function(){
+    var transEffect = Barba.BaseTransition.extend({
+        start: function(){
+          this.newContainerLoading.then(val => this.fadeInNewcontent($(this.newContainer)));
+        },
+        fadeInNewcontent: function(nc) {
+          nc.hide();
+          var _this = this;
+          $(this.oldContainer).fadeOut(1000).promise().done(() => {
+            nc.css('visibility','visible');
+            nc.fadeIn(1000, function(){
+              _this.done();
+            })
+          });
+        }
+    });
+    Barba.Pjax.getTransition = function() {
+      return transEffect;
+    }
+    Barba.Pjax.start();
+  });
 
 const loop = () => {
     const newPixel = window.pageYOffset
